1988a2f67c5aebe8ca6eabcb9653f8a62361b11e
Thisal005 <thisalth2@gmail.com>
2025-12-10 23:09:09 +0530

    Add scheduling support and resource handler for uploads in the application

--- BACKEND/EventTicketingSystem/src/main/java/com/oop/EventTicketingSystem/EventTicketingSystemApplication.java
+++ BACKEND/EventTicketingSystem/src/main/java/com/oop/EventTicketingSystem/EventTicketingSystemApplication.java
@@ -2,8 +2,10 @@ package com.oop.EventTicketingSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EventTicketingSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EventTicketingSystemApplication.class, args);
