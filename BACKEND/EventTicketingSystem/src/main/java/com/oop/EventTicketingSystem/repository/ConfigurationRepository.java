d1263aa949935db5e3ab8c2a4ebb364ec6dd84c7
Thisal005 <thisalth2@gmail.com>
2025-12-11 22:23:50 +0530

    Add ConfigurationRepository and EventRepository interfaces for configuration and event management

---
+++ BACKEND/EventTicketingSystem/src/main/java/com/oop/EventTicketingSystem/repository/ConfigurationRepository.java
@@ -0,0 +1,9 @@
+package com.oop.EventTicketingSystem.repository;
+
+import com.oop.EventTicketingSystem.model.Configuration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConfigurationRepository extends JpaRepository<Configuration, Long> {
+}
---
+++ BACKEND/EventTicketingSystem/src/main/java/com/oop/EventTicketingSystem/repository/EventRepository.java
@@ -0,0 +1,13 @@
+package com.oop.EventTicketingSystem.repository;
+
+import com.oop.EventTicketingSystem.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findByOrganizerId(Long organizerId);
+    List<Event> findByStatus(Event.EventStatus status);
+}
